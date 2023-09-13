9ed73910df07097a8bf6335fcb9681270843368a
Owen Harrington-Woodard <owenhw@gmail.comm>
2023-09-13 13:25:15 -0400

    initial commit

---
+++ files.py
@@ -0,0 +1,15 @@
+import civis
+import os
+from os import listdir
+from os.path import isfile, join
+
+sql = 'select 123'
+
+fut = civis.io.civis_to_csv('temp.csv', sql, 32)
+
+fut.result()
+
+cwd = os.getcwd()
+onlyfiles = [os.path.join(cwd, f) for f in os.listdir(cwd) if 
+os.path.isfile(os.path.join(cwd, f))]
+print(onlyfiles) 
